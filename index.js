3f352ed142de519a94d9553c2caabba7d80c3bf9
Seth David Gyimah <esec26@gmail.com>
2022-06-07 13:30:26 +0000

    phase-1-arithmetric-lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+let num1 = 62
+let num2 = 1
+let num3 = 10
+let num4 = 6
+let multiply = num1 * num2
+let random = Math.floor(Math.random() * 10) + 1;
+let mod = num3 % num4
+let max = Math.max(1, 3, 4, 5, 6, 20)
 
-// Write your code here
